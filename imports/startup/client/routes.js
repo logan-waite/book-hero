bcebde5fccd5d7df0e33789913d234e010de6eaa
Logan Waite <logan@itovi.com>
2018-02-13 15:34:57 -0700

    Super basic layout set up

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,20 +2,16 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import needed templates
-import '../../ui/layouts/body/body.js';
-import '../../ui/pages/home/home.js';
+// Layouts
+import '../../ui/layouts/BaseLayout/BaseLayout.js';
+
+// Templates
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
-  name: 'App.home',
+  name: 'Feed',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
+    BlazeLayout.render('BaseLayout', { main: 'feed' });
   },
 });
-
-FlowRouter.notFound = {
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_notFound' });
-  },
-};
