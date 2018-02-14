b96ba8077d64c56a878a1714678b36561fc7bc62
Logan Waite <logan@itovi.com>
2018-02-14 11:33:55 -0700

    Added basic templates for layout

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -7,11 +7,21 @@ import '../../ui/layouts/BaseLayout/BaseLayout.js';
 
 // Templates
 import '../../ui/pages/not-found/not-found.js';
+import '../../ui/components/profile/profile.js';
+import '../../ui/components/book-list/book-list.js';
+import '../../ui/pages/login/login.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
   name: 'Feed',
   action() {
-    BlazeLayout.render('BaseLayout', { main: 'feed' });
+    BlazeLayout.render('BaseLayout', {left:'profile', main: 'feed', right:'book-list' });
   },
 });
+
+FlowRouter.route('/login', {
+  name: 'Login',
+  action() {
+    BlazeLayout.render('login');
+  }
+})
