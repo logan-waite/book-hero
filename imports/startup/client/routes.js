a0f472f630ccf5aca1fd67451dc5b0d1bde59d13
Logan Waite <logan@itovi.com>
2018-02-21 13:20:33 -0700

    Added Login logic

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,12 +4,44 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 // Import needed templates
 // Layouts
 import '../../ui/layouts/BaseLayout/BaseLayout.js';
+import '../../ui/layouts/AccountLayout/AccountLayout.js';
 
 // Templates
 import '../../ui/pages/not-found/not-found.js';
 import '../../ui/components/profile/profile.js';
 import '../../ui/components/book-list/book-list.js';
-import '../../ui/pages/login/login.js';
+import '../../ui/pages/accounts/accounts.js';
+
+FlowRouter.triggers.enter([AccountsTemplates.ensureSignedIn]);
+
+var pwd = AccountsTemplates.removeField('password');
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+      _id: "username",
+      type: "text",
+      displayName: "username",
+      required: true,
+      minLength: 5,
+  },
+  {
+      _id: 'email',
+      type: 'email',
+      required: true,
+      displayName: "email",
+      re: /.+@(.+){2,}\.(.+){2,}/,
+      errStr: 'Invalid email',
+  },
+  pwd
+]);
+
+AccountsTemplates.configure({
+  defaultLayout: 'AccountLayout',
+  defaultLayoutRegions: {},
+  defaultContentRegion: 'main'
+})
+
+AccountsTemplates.configureRoute('signIn');
 
 // Set up all routes in the app
 FlowRouter.route('/', {
@@ -19,9 +51,16 @@ FlowRouter.route('/', {
   },
 });
 
-FlowRouter.route('/login', {
-  name: 'Login',
+FlowRouter.route('/logout', {
+  name: "Logout",
   action() {
-    BlazeLayout.render('login');
+    AccountsTemplates.logout()
   }
 })
+
+// FlowRouter.route('/login', {
+//   name: 'Login',
+//   action() {
+//     BlazeLayout.render('login');
+//   }
+// })
---
+++ imports/ui/layouts/AccountLayout/AccountLayout.js
@@ -0,0 +1,1 @@
+import './AccountLayout.html';
